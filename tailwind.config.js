955db6f17c3c420f261426c24ecb46a34bff24fc
ferry237 <youbiferry@gmail.com>
2024-03-02 18:51:45 +0100

    my_todo

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config= {
+    theme: {
+      extend: {
+        colors: {
+          'regal-blue': '#243c5a',
+          'bg_color':'bg_color_grad',
+          opatextcolor:'#D1D2DA',
+          blue_color:'#3A7CFD',
+          gray_color:'#9495A5',
+          white_color:'#FFFFFF',
+
+        },
+      }
+    }
+  }
+  
